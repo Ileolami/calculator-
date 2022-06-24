09e2ed0d643bf67bb3f0560837554f0bad844b7a
Ileolami <99335756+Ileolami@users.noreply.github.com>
2022-06-24 16:18:22 +0100

    Create life.js

---
+++ life.js
@@ -0,0 +1,33 @@
+let operator =prompt("Enter your operator(+,-,/,*)");
+
+let firstvalue =parseInt(prompt("Enter your first number"));
+
+let secondvalue =parseInt(prompt("Enter your second number"));
+
+const result = (a, b) =>{
+
+  if(operator =="+") {
+
+    return a + b;
+
+  } else if (operator == "_") {
+
+    return a - b;
+
+  } else if (operator == "/") {
+
+    return a / b;
+
+  } else if (operator == "*") {
+
+    return a * b;
+
+  } else {
+
+    return "invalid operator, Try again!!!";
+
+  }
+
+};
+
+alert( result(firstvalue, secondvalue));
